eba99b32a440a692954d8645d5deb6ca9c5fad90
ysKim <znxkznxk1030@gmail.com>
2021-10-22 14:13:10 +0530

    api| init product

--- microservices/api/src/main/java/arthur/kim/microservices/core/api/product/Product.java
+++ microservices/api/src/main/java/arthur/kim/microservices/core/api/product/Product.java
@@ -1,4 +1,31 @@
 package arthur.kim.microservices.core.api.product;
 
 public class Product {
+    private final int productId;
+    private final String name;
+    private final int weight;
+    private final String serviceAddress;
+
+    public Product(int productId, String name, int weight, String serviceAddress) {
+        this.productId = productId;
+        this.name = name;
+        this.weight = weight;
+        this.serviceAddress = serviceAddress;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getServiceAddress() {
+        return serviceAddress;
+    }
 }
--- microservices/api/src/main/java/arthur/kim/microservices/core/api/product/ProductService.java
+++ microservices/api/src/main/java/arthur/kim/microservices/core/api/product/ProductService.java
@@ -1,4 +1,12 @@
 package arthur.kim.microservices.core.api.product;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public interface ProductService {
+    @GetMapping(
+            value = "/product/{productId}",
+            produces = "application/json"
+    )
+    Product getProduct(@PathVariable int productId);
 }
