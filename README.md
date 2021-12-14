f3831724d227e3827e76514db1181e2dec59095e
ysKim <znxkznxk1030@gmail.com>
2021-12-15 01:31:57 +0900

    Pruduct | 영속성 테스트 3

--- README.md
+++ README.md
@@ -1017,6 +1017,56 @@ spring.data.mongodb:
     auto-index-creation: true
 ```
 
-#### optimisticLockError 테스트
+#### 낙관적 잠금 메커니즘 테스트
 
+```java
+@Test
+ public void optimisticLockError() {
+  
+  // 데이터베이스에서 가져온 엔티티를 변수 2개에 저장한다.
+  ProductEntity entity1 = repository.findById(savedEntity.getId()).get();
+  ProductEntity entity2 = repository.findById(savedEntity.getId()).get();
+  
+  // 첫 번째 엔티티 객체를 업데이트 한다.
+  entity1.setName("n1");
+  repository.save(entity1);
+  
+  // 두 번째 엔티티 객체를 업데이트한다.
+  // 두 번째 엔티티 객체의 버전이 낮으므로 실패한다.
+  // 즉 낙관적 잠금 오류가 발생해 실패한다.
+  try {
+   entity2.setName("n2");
+   repository.save(entity2);
+   
+   fail("Expected an OptimisticLockingFailureException");
+  } catch (OptimisticLockingFailureException e) {}
+  
+  // 데이터베이스에서 업데이트된 엔티티를 가져와서 새로운 값을 확인한다.
+  ProductEntity updatedEntity = repository.findById(savedEntity.getId()).get();
+  assertEquals(1, (int)updatedEntity.getVersion());
+  assertEquals("n1", updatedEntity.getName());
+ }
+```
 
+#### paging 테스스
+
+```java
+@Test
+ public void paging() {
+  repository.deleteAll();
+  List<ProductEntity> newProducts = rangeClosed(1001, 1010)
+    .mapToObj(i -> new ProductEntity(i, "name " + i, i)).collect(Collectors.toList());
+  repository.saveAll(newProducts);
+  
+  PageRequest nextPage = PageRequest.of(0, 4, Direction.ASC, "productId");
+  Page<ProductEntity> productPage = repository.findAll(nextPage);
+  assertEquals("[1001, 1002, 1003, 1004]", productPage.getContent().stream().map(p -> p.getProductId()).collect(Collectors.toList()).toString());
+  assertEquals(true, productPage.hasNext());
+//  nextPage = testNextPage(nextPage, "[1001, 1002, 1003, 1004]", true);
+//  nextPage = testNextPage(nextPage, "[1005, 1006, 1007, 1008]", true);
+//  nextPage = testNextPage(nextPage, "[1009, 1010]", false);
+    
+ }
+```
+
+![영속성 테스트 2](./screen-shot/persistence-test-2.png)
--- microservices/product-service/src/test/java/arthur/kim/microservices/core/product/PersistenceTests.java
+++ microservices/product-service/src/test/java/arthur/kim/microservices/core/product/PersistenceTests.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,14 +16,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import arthur.kim.microservices.core.product.persistence.ProductEntity;
 import arthur.kim.microservices.core.product.persistence.ProductRepository;
 
+import static java.util.stream.IntStream.rangeClosed;
+
 @DataMongoTest
 //@TestInstance(Lifecycle.PER_CLASS)
+@EnableSpringDataWebSupport
 public class PersistenceTests {
 	
 	@Autowired
@@ -79,6 +91,57 @@ public class PersistenceTests {
 			ProductEntity entity2 = new ProductEntity(savedEntity.getProductId(), "n", 1);
 			repository.save(entity2);
 		});
+	}
+	
+	@Test
+	public void optimisticLockError() {
+		
+		// 데이터베이스에서 가져온 엔티티를 변수 2개에 저장한다.
+		ProductEntity entity1 = repository.findById(savedEntity.getId()).get();
+		ProductEntity entity2 = repository.findById(savedEntity.getId()).get();
+		
+		// 첫 번째 엔티티 객체를 업데이트 한다.
+		entity1.setName("n1");
+		repository.save(entity1);
+		
+		// 두 번째 엔티티 객체를 업데이트한다.
+		// 두 번째 엔티티 객체의 버전이 낮으므로 실패한다.
+		// 즉 낙관적 잠금 오류가 발생해 실패한다.
+		try {
+			entity2.setName("n2");
+			repository.save(entity2);
+			
+			fail("Expected an OptimisticLockingFailureException");
+		} catch (OptimisticLockingFailureException e) {}
+		
+		// 데이터베이스에서 업데이트된 엔티티를 가져와서 새로운 값을 확인한다.
+		ProductEntity updatedEntity = repository.findById(savedEntity.getId()).get();
+		assertEquals(1, (int)updatedEntity.getVersion());
+		assertEquals("n1", updatedEntity.getName());
+	}
+	
+	@Test
+	public void paging() {
+		repository.deleteAll();
+		List<ProductEntity> newProducts = rangeClosed(1001, 1010)
+				.mapToObj(i -> new ProductEntity(i, "name " + i, i)).collect(Collectors.toList());
+		repository.saveAll(newProducts);
+		
+		PageRequest nextPage = PageRequest.of(0, 4, Direction.ASC, "productId");
+		Page<ProductEntity> productPage = repository.findAll(nextPage);
+		assertEquals("[1001, 1002, 1003, 1004]", productPage.getContent().stream().map(p -> p.getProductId()).collect(Collectors.toList()).toString());
+		assertEquals(true, productPage.hasNext());
+//		nextPage = testNextPage(nextPage, "[1001, 1002, 1003, 1004]", true);
+//		nextPage = testNextPage(nextPage, "[1005, 1006, 1007, 1008]", true);
+//		nextPage = testNextPage(nextPage, "[1009, 1010]", false);
+				
+	}
+	
+	private PageRequest testNextPage(PageRequest nextPage, String expectedProductIds, boolean expectsNextPage) {
+		Page<ProductEntity> productPage = repository.findAll(nextPage);
+		assertEquals(expectedProductIds, productPage.getContent().stream().map(p -> p.getProductId()).collect(Collectors.toList()).toString());
+		assertEquals(expectsNextPage, productPage.hasNext());
+		return (PageRequest) productPage.nextPageable();
 	}
 	
 	
