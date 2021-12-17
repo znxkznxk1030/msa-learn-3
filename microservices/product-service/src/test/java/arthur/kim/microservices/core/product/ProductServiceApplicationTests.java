ee4a3ca8e3a349df91e477accddfc7ca093f093f
ysKim <znxkznxk1030@gmail.com>
2021-12-17 23:37:30 +0900

    ProductServiceApplicationTests | get, post, delete

--- microservices/product-service/src/test/java/arthur/kim/microservices/core/product/ProductServiceApplicationTests.java
+++ microservices/product-service/src/test/java/arthur/kim/microservices/core/product/ProductServiceApplicationTests.java
@@ -1,13 +1,98 @@
 package arthur.kim.microservices.core.product;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import arthur.kim.api.product.Product;
+import arthur.kim.microservices.core.product.persistence.ProductRepository;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static reactor.core.publisher.Mono.just;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
+
+  @Autowired
+  private WebTestClient client;
+
+  @Autowired
+  private ProductRepository repository;
 
 	@Test
 	void contextLoads() {
 	}
 
+  @BeforeAll
+  public void setupDb() {
+    repository.deleteAll();
+  }
+
+  @Test
+  public void getProductById() {
+
+  }
+
+  @Test
+  public void duplicatedError() {
+
+  }
+
+  @Test
+  public void deleteProduct() {
+
+  }
+
+  @Test
+  public void getProductInvalidParameterString() {
+
+  }
+
+  @Test
+  public void getProductNotFound() {
+
+  }
+
+  @Test
+  public void getProductInvalidParameterNegativeValue() {
+
+  }
+
+  private WebTestClient.BodyContentSpec getAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+    return getAndVerifyProduct("/" + productId, expectedStatus);
+  }
+
+  private WebTestClient.BodyContentSpec getAndVerifyProduct(String productIdPath, HttpStatus expectedStatus) {
+    return client.get()
+      .uri("/product" + productIdPath)
+      .accept(APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isEqualTo(expectedStatus)
+      .expectHeader().contentType(APPLICATION_JSON)
+      .expectBody();
+  }
+
+  private WebTestClient.BodyContentSpec postAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+    Product product = new Product(productId, "Name " + productId, productId, "SA");
+    return client.post().uri("/product")
+        .body(just(product), Product.class)
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isEqualTo(expectedStatus)
+        .expectHeader().contentType(APPLICATION_JSON)
+        .expectBody();
+  }
+
+  private WebTestClient.BodyContentSpec deleteAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+    return client.post().uri("/product/" + productId)
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isEqualTo(expectedStatus)
+        .expectBody();
+  }
 }
