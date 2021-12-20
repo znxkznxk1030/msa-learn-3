6b0edc69424b47bbc3a1304f52841c466143138a
ysKim <znxkznxk1030@gmail.com>
2021-12-20 23:38:47 +0900

    deleteCompositeProduct #2

--- microservices/product-composite-service/src/main/java/arthur/kim/microservices/core/product/composite/services/ProductCompositeServiceImpl.java
+++ microservices/product-composite-service/src/main/java/arthur/kim/microservices/core/product/composite/services/ProductCompositeServiceImpl.java
@@ -102,7 +102,7 @@ public class ProductCompositeServiceImpl implements ProductCompositeService {
     // 3. Copy summary review info, if available
     List<ReviewSummary> reviewSummaries = (reviews == null) ? null
         : reviews.stream()
-            .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject()))
+            .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject(), r.getContent()))
             .collect(Collectors.toList());
 
     // 4. Create info regarding the involved microservices addresses
