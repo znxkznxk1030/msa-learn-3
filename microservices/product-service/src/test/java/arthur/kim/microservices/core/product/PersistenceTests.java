54ae905fcdf0b8fc90e90d21d610eb1a82b97f68
ysKim <znxkznxk1030@gmail.com>
2022-01-06 00:13:38 +0900

    Product | PersistenceTest Flux 적용

--- microservices/product-service/src/test/java/arthur/kim/microservices/core/product/PersistenceTests.java
+++ microservices/product-service/src/test/java/arthur/kim/microservices/core/product/PersistenceTests.java
@@ -1,6 +1,6 @@
 package arthur.kim.microservices.core.product;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -45,7 +45,7 @@ public class PersistenceTests {
 
   @BeforeEach
   public void setupDb() {
-    StepVerifier.create(repository.deleteAll()).verifyComplete();
+	StepVerifier.create(repository.deleteAll()).verifyComplete();
 
     ProductEntity entity = new ProductEntity(1, "n", 1);
     StepVerifier.create(repository.save(entity))
@@ -55,31 +55,39 @@ public class PersistenceTests {
         }).verifyComplete();
   }
 
-  // @Test
-  // public void create() {
-  //   ProductEntity newEntity = new ProductEntity(2, "n", 2);
+   @Test
+   public void create() {
+     ProductEntity newEntity = new ProductEntity(2, "n", 2);
+     
+     StepVerifier.create(repository.save(newEntity))
+     	.expectNextMatches(createdEntity -> newEntity.getProductId() == createdEntity.getProductId())
+     	.verifyComplete();
+     
+     
+     StepVerifier.create(repository.findById(newEntity.getId()))
+     	.expectNextMatches(foundEntity -> areProductEqual(foundEntity, newEntity))
+     	.verifyComplete();
+     
+     StepVerifier.create(repository.count()).expectNext(2l).verifyComplete();
+   }
 
-  //   ProductEntity foundEntity = repository.findById(newEntity.getId()).get();
-  //   assertEqualsProduct(newEntity, foundEntity);
+   @Test
+   public void update() {
+	   savedEntity.setName("n2");
+	   StepVerifier.create(repository.save(savedEntity))
+	   .expectNextMatches(updatedEntity -> updatedEntity.getName().equals("n2"))
+	   .verifyComplete();
+	   
+	   StepVerifier.create(repository.findById(savedEntity.getId()))
+	   .expectNextMatches(foundEntity -> foundEntity.getVersion() == 1 && foundEntity.getName().equals("n2"))
+	   .verifyComplete();
+   }
 
-  //   assertEquals(2, repository.count());
-  // }
-
-  // @Test
-  // public void update() {
-  //   savedEntity.setName("n2");
-  //   repository.save(savedEntity);
-
-  //   ProductEntity foundEntity = repository.findById(savedEntity.getId()).get();
-
-  //   assertEquals(1, (long) foundEntity.getVersion());
-  //   assertEquals("n2", foundEntity.getName());
-  // }
-
-  // @Test
-  // public void delete() {
-  //   repository.delete(savedEntity);
-  // }
+   @Test
+   public void delete() {
+	   StepVerifier.create(repository.delete(savedEntity)).verifyComplete();
+	   StepVerifier.create(repository.existsById(savedEntity.getId())).expectNext(false).verifyComplete();
+   }
 
   @Test
   public void getByProductId() {
@@ -88,67 +96,34 @@ public class PersistenceTests {
         .verifyComplete();
   }
 
-  // @Test
-  // public void duplicateError() {
-  //   assertThrows(DuplicateKeyException.class, () -> {
-  //     ProductEntity entity2 = new ProductEntity(savedEntity.getProductId(), "n", 1);
-  //     repository.save(entity2);
-  //   });
-  // }
+   @Test
+   public void duplicateError() {
+	   ProductEntity entity = new ProductEntity(savedEntity.getProductId(), "n", 1);
+	   StepVerifier.create(repository.save(entity)).expectError(DuplicateKeyException.class).verify();
+   }
 
-  // @Test
-  // public void optimisticLockError() {
+   @Test
+   public void optimisticLockError() {
 
-  //   // 데이터베이스에서 가져온 엔티티를 변수 2개에 저장한다.
-  //   ProductEntity entity1 = repository.findById(savedEntity.getId()).get();
-  //   ProductEntity entity2 = repository.findById(savedEntity.getId()).get();
+     // 데이터베이스에서 가져온 엔티티를 변수 2개에 저장한다.
+     ProductEntity entity1 = repository.findById(savedEntity.getId()).block();
+     ProductEntity entity2 = repository.findById(savedEntity.getId()).block();
 
-  //   // 첫 번째 엔티티 객체를 업데이트 한다.
-  //   entity1.setName("n1");
-  //   repository.save(entity1);
+     // 첫 번째 엔티티 객체를 업데이트 한다.
+     entity1.setName("n1");
+     repository.save(entity1).block();
 
-  //   // 두 번째 엔티티 객체를 업데이트한다.
-  //   // 두 번째 엔티티 객체의 버전이 낮으므로 실패한다.
-  //   // 즉 낙관적 잠금 오류가 발생해 실패한다.
-  //   try {
-  //     entity2.setName("n2");
-  //     repository.save(entity2);
+     // 두 번째 엔티티 객체를 업데이트한다.
+     // 두 번째 엔티티 객체의 버전이 낮으므로 실패한다.
+     // 즉 낙관적 잠금 오류가 발생해 실패한다.
+     StepVerifier.create(repository.save(entity2)).expectError(OptimisticLockingFailureException.class).verify();
+     
+     StepVerifier.create(repository.findById(savedEntity.getId()))
+     .expectNextMatches(foundEntity -> 
+    		 foundEntity.getVersion() == 1 && foundEntity.getName().equals("n1"))
+     .verifyComplete();
+   }
 
-  //     fail("Expected an OptimisticLockingFailureException");
-  //   } catch (OptimisticLockingFailureException e) {
-  //   }
-
-  //   // 데이터베이스에서 업데이트된 엔티티를 가져와서 새로운 값을 확인한다.
-  //   ProductEntity updatedEntity = repository.findById(savedEntity.getId()).get();
-  //   assertEquals(1, (int) updatedEntity.getVersion());
-  //   assertEquals("n1", updatedEntity.getName());
-  // }
-
-  // @Test
-  // public void paging() {
-  //   repository.deleteAll();
-  //   List<ProductEntity> newProducts = rangeClosed(1001, 1010)
-  //       .mapToObj(i -> new ProductEntity(i, "name " + i, i)).collect(Collectors.toList());
-  //   repository.saveAll(newProducts);
-
-  //   PageRequest nextPage = PageRequest.of(0, 4, Direction.ASC, "productId");
-  //   Page<ProductEntity> productPage = repository.findAll(nextPage);
-  //   assertEquals("[1001, 1002, 1003, 1004]",
-  //       productPage.getContent().stream().map(p -> p.getProductId()).collect(Collectors.toList()).toString());
-  //   assertEquals(true, productPage.hasNext());
-  //   nextPage = testNextPage(nextPage, "[1001, 1002, 1003, 1004]", true);
-  //   nextPage = testNextPage(nextPage, "[1005, 1006, 1007, 1008]", true);
-  //   nextPage = testNextPage(nextPage, "[1009, 1010]", false);
-
-  // }
-
-  // private void assertEqualsProduct(ProductEntity expectedEntity, ProductEntity actualEntity) {
-  //   assertEquals(expectedEntity.getId(), actualEntity.getId());
-  //   assertEquals(expectedEntity.getVersion(), actualEntity.getVersion());
-  //   assertEquals(expectedEntity.getProductId(), actualEntity.getProductId());
-  //   assertEquals(expectedEntity.getName(), actualEntity.getName());
-  //   assertEquals(expectedEntity.getWeight(), actualEntity.getWeight());
-  // }
 
   private boolean areProductEqual(ProductEntity expectedEntity, ProductEntity actualEntity) {
     return (expectedEntity.getId().equals(actualEntity.getId())) &&
